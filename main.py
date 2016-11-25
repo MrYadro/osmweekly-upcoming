ef6cc70ee40a5e372cf2f637278b7cb36bbb2255
MrYadro <yadrishnikovyaroslav@gmail.com>
2016-11-25 17:40:01 +0700

    fix row skip

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import csv
 import requests
 
 FILENAME = 'in.txt'
-GEONAMES_USERNAME = 'demo'
+GEONAMES_USERNAME = 'Demo'
 LANG = 'ru'
-SKIP = ['Where     ', '----------']
 
 with open(FILENAME, 'r', newline='') as incsvfile:
     with open(LANG + '_' + FILENAME, 'w', newline='') as outcsvfile:
         osmweeklyreader = csv.reader(incsvfile, delimiter='|')
         osmweeklywriter = csv.writer(outcsvfile, delimiter='|')
+        rownum = 0
         for row in osmweeklyreader:
-            if row [1] not in SKIP:
+            if row [1] and rownum > 1:
                 r = requests.get('http://api.geonames.org/searchJSON?q=' + row [1] + '&lang=' + LANG + '&username=' + GEONAMES_USERNAME)
                 pew = r.json().get('geonames')[0].get('name')
                 row [1] = pew
             print (row [1])
             osmweeklywriter.writerow(row)
+            rownum += 1
